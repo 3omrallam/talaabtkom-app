1aa84a981611e5c85228c4d52c1ab5c13cf2e1c5
3omr Allam <76155662+3omrallam@users.noreply.github.com>
2022-01-09 14:34:15 +0200

    LunTeam v0.1

--- src/app/view/layout/side-menu-layout/side-menu-layout.component.ts
+++ src/app/view/layout/side-menu-layout/side-menu-layout.component.ts
@@ -9,10 +9,10 @@ import { UserProfileAsidePageService } from '../../pages/services/user-profile-a
 })
 export class SideMenuLayoutComponent implements OnInit {
 
-  constructor(public activeRouter: Router, public _UserProfileAsidePageService: UserProfileAsidePageService) { }
+  constructor(public activeRouter: Router,public _UserProfileAsidePageService: UserProfileAsidePageService) { }
 
   ngOnInit(): void {
-    console.log(this.activeRouter.url.includes('/resturants'));
+    console.log(this.activeRouter);
     
     console.log(this._UserProfileAsidePageService.userProfileNavigator);
   }
