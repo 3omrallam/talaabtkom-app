0d91a5c7dcade07a937fc1a9ade4d439d43f4cc8
Nada Ahmed <66229072+nadaahmed563@users.noreply.github.com>
2022-01-11 11:01:50 +0200

    LunTeam v0.1

--- src/app/view/sections/user-reservations-section/user-reservations-section.component.ts
+++ src/app/view/sections/user-reservations-section/user-reservations-section.component.ts
@@ -10,22 +10,54 @@ import { ReservationsListService} from '../services/reservations-list.service'
 })
 export class UserReservationsSectionComponent implements OnInit {
   reservationsList: ReservationList[] = [];
+  btn = "CC-default"
+  btn1 = "CC-default";
+  btn2 = "CC-default";
+  btn3 = "CC-default";
  
 
-  constructor(public _ReservationsListService: ReservationsListService, public _Router: Router) { }
+  constructor(public _ReservationsListService: ReservationsListService, public _Router: Router,public activeRouter: Router) { }
 
   ngOnInit(): void {
     this.allFilter();
+    this.btn = "CC-active"
+    this.btn1 = "CC-default";
+    this.btn2 = "CC-default";
+    this.btn3 = "CC-default";
   }
 
 
-  // Reservations Filteration
+  // All Reservations 
   allFilter(){
     this.reservationsList = this._ReservationsListService.reservationsList;
+    this.btn = "CC-active"
+    this.btn1 = "CC-default";
+    this.btn2 = "CC-default";
+    this.btn3 = "CC-default";
   }
+  // Reservations Filteration
   Filter(FilterName:any){
     this.reservationsList = this._ReservationsListService.reservationsList.filter(ele => ele.process == FilterName)
     return this.reservationsList;
+  }
+  // Styling
+  div1Function() {
+    this.btn = "CC-default"
+    this.btn1 = "CC-active";
+    this.btn2 = "CC-default";
+    this.btn3 = "CC-default";
+  }
+  div2Function() {
+    this.btn = "CC-default"
+    this.btn1 = "CC-default";
+    this.btn2 = "CC-active";
+    this.btn3 = "CC-default";
+  }
+  div3Function() {
+    this.btn = "CC-default"
+    this.btn1 = "CC-default";
+    this.btn2 = "CC-default";
+    this.btn3 = "CC-active";
   }
 
 }
